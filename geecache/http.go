6e8b26c3779efb8a596a6524ae46cb750815677a
Hero <2861061959@qq.com>
2025-05-25 15:47:01 +0800

    feat: add distributed cache

--- geecache/http.go
+++ geecache/http.go
@@ -1,17 +1,31 @@
 package geecache
 
 import (
+	"bee-web/geecache/consistenthash"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
+	"sync"
 )
 
-const defaultBasePath = "/_geecache/"
+const (
+	defaultBasePath = "/_geecache/"
+	defaultReplicas = 50
+)
 
 type HTTPPool struct {
-	self     string // 自己的地址
-	basePath string // 节点通讯地址的前缀
+	self       string // 自己的地址
+	basePath   string // 节点通讯地址的前缀
+	mu         sync.Mutex
+	peers      *consistenthash.Map // 一致性哈希算法
+	httpGetter map[string]*httpGetter
+}
+
+type httpGetter struct {
+	baseURL string
 }
 
 func newHTTPPool(self string) *HTTPPool {
@@ -53,3 +67,48 @@ func (p *HTTPPool) ServerHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Write(view.ByteSlice())
 }
+
+func (h *httpGetter) Get(group string, key string) ([]byte, error) {
+	u := fmt.Sprintf("%v%v/%v", h.baseURL, url.QueryEscape(group), url.QueryEscape(key))
+	res, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server returned: %v", res.Status)
+	}
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	return bytes, nil
+}
+
+var _ PeerGetter = (*httpGetter)(nil)
+
+func (p *HTTPPool) Set(peers ...string) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.peers = consistenthash.New(defaultReplicas, nil)
+	p.peers.Add(peers...)
+	p.httpGetter = make(map[string]*httpGetter, len(peers))
+	for _, peer := range peers {
+		p.httpGetter[peer] = &httpGetter{
+			baseURL: peer + p.basePath,
+		}
+	}
+}
+
+func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if peer := p.peers.Get(key); peer != "" && peer != p.self {
+		p.Log("Pick peer %s", peer)
+		return p.httpGetter[peer], true
+	}
+	return nil, false
+}
+
+var _ PeerPicker = (*HTTPPool)(nil)
